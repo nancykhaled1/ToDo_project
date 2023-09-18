affb92a64b3521874263bc54b5a55e8edfe69bd7
nancy khaled <nancykhaledn905@gmail.com>
2023-09-18 05:01:24 +0200

    initial project.

---
+++ lib/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeNam = 'home_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:project_todo/home_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeNam,
+      routes: {
+        HomeScreen.routeNam: (context) => HomeScreen(),
+
+      },
+    );
+  }
+}
